b4d772a081b4ec445553bf62df7f0b036b7ed65f
agua-2025 <vivendamirassol@gmail.com>
2025-08-10 19:24:36 -0400

    Simplifica startup para depuração do erro 502

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Parte 1: Configura o servidor web (Nginx)
+# Configura o servidor web Nginx (Isso é rápido e deve ficar)
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 service nginx restart
 
-# Parte 2: Configura a aplicação Laravel (nossa solução)
-cd /home/site/wwwroot
-php artisan key:generate --force
-php artisan config:cache
-php artisan migrate --force
+# Comandos do Laravel - DESATIVADOS TEMPORARIAMENTE
+# Vamos executá-los manualmente depois que o servidor estiver estável.
+# cd /home/site/wwwroot
+# php artisan key:generate --force
+# php artisan config:cache
+# php artisan migrate --force
