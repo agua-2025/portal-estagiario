4e9cc2b5d97e2b363b1afb8c54a03b4aa7f9a8bc
agua-2025 <vivendamirassol@gmail.com>
2025-08-15 00:04:20 -0400

    trocando calendário222

--- resources/views/candidato/profile/edit.blade.php
+++ resources/views/candidato/profile/edit.blade.php
@@ -200,10 +200,12 @@
                         sum = 0;
                         for (let i = 1; i <= 10; i++) sum += parseInt(cleanedCpf.substring(i-1, i)) * (12 - i);
                         rest = (sum * 10) % 11;
-                        if (rest === 10 || rest === 11) return 0;
+                        if (rest === 10 || rest === 11) rest = 0;
                         if (rest !== parseInt(cleanedCpf.substring(10, 11))) return false;
+
                         return true;
                         }
+
                     };
                     }
                     </script>
